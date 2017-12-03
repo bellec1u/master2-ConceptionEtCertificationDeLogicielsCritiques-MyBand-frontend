50f98e7fc63f5e00f69742c1266fd57b828e9b30
Djenkal <djenkal.karim@gmail.com>
2017-12-03 14:04:52 +0100

    app

---
+++ src/app/app.routes.ts
@@ -0,0 +1,16 @@
+import { RouterModule, Routes } from '@angular/router';
+
+// APP COMPONENTS
+import { HomeComponent } from './home/home.component';
+import { ArtistsComponent } from './artists/artists.component';
+import { BandsComponent } from './bands/bands.component';
+
+const ROUTES: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'artists', component: ArtistsComponent },
+  { path: 'bands', component: BandsComponent },
+  { path: 'artist/:id', component: ArtistsComponent }
+];
+
+export const APP_ROUTES = RouterModule.forRoot(ROUTES, { useHash: true });
---
+++ src/app/artist/artist.component.ts
@@ -0,0 +1,75 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ArtistsService } from '../shared/artists-service/artists.service';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/merge';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/mergeMap';
+import 'rxjs/add/operator/do';
+
+@Component({
+  selector: 'app-artist',
+  templateUrl: './artist.component.html',
+  styleUrls: ['./artist.component.css']
+})
+export class ArtistComponent implements OnInit {
+
+  // private property to store artist value
+  private _artist: any;
+  // private property to store flag to know if it's a person
+  private _isPerson: boolean;
+
+  /**
+   * Component constructor
+   */
+  constructor(private _artistsService: ArtistsService, private _route: ActivatedRoute) {
+    this._artist = {};
+    this._isPerson = false;
+  }
+
+  /**
+   * Returns private property _artist
+   *
+   * @returns {any}
+   */
+  get artist(): any {
+    return this._artist;
+  }
+
+  /**
+   * Returns flag to know if we are on a profile or on HP
+   *
+   * @returns {boolean}
+   */
+  get isPerson(): boolean {
+    return this._isPerson;
+  }
+
+  /**
+   * OnInit implementation
+   */
+  ngOnInit() {
+    Observable
+      .merge(
+        this._route.params
+          .filter(params => !!params['id'])
+          .flatMap(params => this._artistsService.fetchOne(params['id']))
+          .do(_ => this._isPerson = true),
+        this._route.params
+          .filter(params => !params['id'])
+          .flatMap(_ => this._artistsService.fetchRandom())
+          .do(_ => this._isPerson = false)
+      )
+      .subscribe((artist: any) => this._artist = artist);
+  }
+
+  /**
+   * Returns random people
+   */
+  random() {
+    this._artistsService
+      .fetchRandom()
+      .subscribe((artist: any) => this._artist = artist);
+  }
+
+}
