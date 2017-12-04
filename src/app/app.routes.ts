e39ac298016ee3f9886e0e4533f61c82c9d6b1d3
Djenkal <djenkal.karim@gmail.com>
2017-12-04 16:38:02 +0100

    Artist profile

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,12 +5,13 @@ import { HomeComponent } from './home/home.component';
 import { ArtistsComponent } from './artists/artists.component';
 import { BandsComponent } from './bands/bands.component';
 import { BandComponent } from './band/band.component';
+import {ArtistComponent} from './artist/artist.component';
 
 const ROUTES: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'artists', component: ArtistsComponent },
-  { path: 'artist/:id', component: ArtistsComponent },
+  { path: 'artist/:id', component: ArtistComponent },
   { path: 'bands', component: BandsComponent },
   { path: 'band/:id', component: BandComponent }
 ];
